76383699404482fb57189bdac71735efd905ea67
Anonimsko <96410485+Anonimsko@users.noreply.github.com>
2023-04-05 12:16:50 +0200

    Added scrollable table and busted page

---
+++ busted/script.js
@@ -0,0 +1,42 @@
+function getAndDisplayData()
+{
+    fetch("https://servers-frontend.fivem.net/api/servers/single/rma857", { cache: "no-cache" })
+    .then(response => response.json())
+    .then(data => 
+    {
+        let playerList = document.getElementById("playerList");
+        let onlinePlayers = document.getElementById("onlinePlayers");
+
+        let playersData = data.Data.players;
+        onlinePlayers.innerHTML = `Online Players: ${playersData.length}`;
+        playerList.innerHTML = "";
+
+        playersData.forEach(player => 
+        {
+            let row = document.createElement("tr");
+            let nick = document.createElement("td");
+            let discord = document.createElement("td");
+            let license = document.createElement("td");
+            let discordLink = document.createElement("a");
+            discord.appendChild(discordLink);
+
+            nick.innerText = player.name;
+            license.innerText = player.identifiers.find(id => id.startsWith("license:")).substr(8);
+            let discordID = player.identifiers.find(id => id.startsWith("discord:"));
+            discordLink.innerText = discordID ? discordID.substr(8) : "Not Linked";
+            if (discordID && discordID != "Not Linked")
+            {
+                discordLink.setAttribute("href", `https://discord.id/?prefill=${discordID.substr(8)}`);
+                discordLink.setAttribute("target", "blank");
+            }
+
+            row.appendChild(nick);
+            row.appendChild(discord);
+            row.appendChild(license);
+            playerList.appendChild(row);
+        });
+    }).catch(error => console.log(error));
+}
+
+getAndDisplayData()
+setInterval(getAndDisplayData, 10000);
